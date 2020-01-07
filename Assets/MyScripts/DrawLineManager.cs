17e1930c5e2567fb7e40719e6de865456d304a53
Bryan Lopes <brylopes89@gmail.com>
2020-01-07 15:39:45 -0500

    Initial commit

---
+++ Assets/MyScripts/DrawLineManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLineManager : MonoBehaviour
+{
+    Public SteamVR_TrackedObject trackedObj;
+
+    void Update()
+    {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+
+        }
+    }
+}
