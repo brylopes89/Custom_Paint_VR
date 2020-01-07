6f8efa9625bd265fa542440a2f55671d2885f9ed
Bryan Lopes <brylopes89@gmail.com>
2020-01-07 16:03:45 -0500

    Added DrawLine Manager script

--- Assets/MyScripts/DrawLineManager.cs
+++ Assets/MyScripts/DrawLineManager.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class DrawLineManager : MonoBehaviour
 {
-    Public SteamVR_TrackedObject trackedObj;
+    public SteamVR_TrackedObject trackedObj;
 
     void Update()
     {
         SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
         if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
         {
-
+            debug.Log("Got Here");
         }
     }
 }
