8d215ba86e54d9bfa7bdcca1739e302f081840b6
Bryan Lopes <brylopes89@gmail.com>
2020-01-22 14:10:35 -0500

    skybox asset pack added

--- Assets/MyScripts/DrawLineManager.cs
+++ Assets/MyScripts/DrawLineManager.cs
@@ -10,7 +10,7 @@ public class DrawLineManager : MonoBehaviour
     public GameObject penPoint;
     public GameObject menu;
     public Hand leftHand;
-    public SteamVR_Behaviour_Pose trackedObj;   
+    public SteamVR_Behaviour_Pose trackedObj;    
     public SteamVR_Action_Boolean trackPadAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("InteractUI");
     public SteamVR_Action_Vibration hapticAction;
     public Material lMat;
@@ -24,7 +24,8 @@ public class DrawLineManager : MonoBehaviour
 
     public void Awake()
     {
-        Instance = this;        
+        Instance = this;      
+        
     }
 
     private void Start()
@@ -54,12 +55,11 @@ public class DrawLineManager : MonoBehaviour
                 lineRendererList.Add(go);
             }
             else if (trackPadAction.GetState(trackedObj.inputSource))
-            {
-                //currLine.positionCount = numClicks + 1;
-                //currLine.SetPosition(numClicks, trackedObj.transform.position);
-                currLine.AddPoint(penPoint.transform.position, playerTrans.position);
-                numClicks++;
+            {              
+                //Vector3 points = penPoint
+                currLine.AddPoint(penPoint.transform.position, playerTrans.position); //trackedObj.transform.position
 
+                numClicks++;
             }
             else if (trackPadAction.GetStateUp(trackedObj.inputSource))
             {
@@ -85,6 +85,7 @@ public class DrawLineManager : MonoBehaviour
     public void SetMenuPosition(GameObject menu)
     {        
         GameObject game = menu.transform.parent.gameObject;
+       
         game.transform.parent = leftHand.transform;
         game.transform.localRotation = Quaternion.Euler(new Vector3(55f, leftHand.transform.rotation.eulerAngles.y, 0));
         game.transform.localPosition = new Vector3(0.2f, 0, 0);
--- Assets/MyScripts/SerialUISummoner.cs
+++ Assets/MyScripts/SerialUISummoner.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class SerialUISummoner : MonoBehaviour
 {
-    public float minDistance = 2;
-    public float delay = 0.5f;
+    public float minDistance = .2f;
+    public float delay = 0.9f;
     public bool showing = false;   
     public Transform player;
-    
+    public SteamVR_Behaviour_Pose trackedObj;
+    public SteamVR_Action_Boolean trackPadAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("InteractUI");
+
     protected Animator[] children;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
-    {        
+    {
+        anim = GetComponent<Animator>();
         children = GetComponentsInChildren<Animator>();
+
+        anim.SetBool("Shown", showing);
         for (int a = 0; a < children.Length; a++)
         {
             children[a].SetBool("Shown", showing);
@@ -25,23 +32,40 @@ public class SerialUISummoner : MonoBehaviour
     void Update()
     {
         Vector3 delta = Camera.main.transform.position - transform.position;
-        
-        if(delta.magnitude < minDistance)
+        float distanceSquare = minDistance * minDistance;
+        /*if(delta.sqrMagnitude < distanceSquare)
         {
             if (showing) return;            
-            StartCoroutine(ActivateInTurn());           
+            StartCoroutine(ActivateInTurn());
+            
         }
         else
         {
             if (!showing) return;
             StartCoroutine("DeactivateInTurn");
-        }       
+        }*/
+
+        if (trackPadAction.GetStateDown(trackedObj.inputSource))
+        {
+            if (showing) return;
+            StartCoroutine(ActivateInTurn());
+        }        
+        else if (trackPadAction.GetStateUp(trackedObj.inputSource))
+        {
+            if (!showing) return;
+            StartCoroutine("DeactivateInTurn");
+        }
     }
     public IEnumerator ActivateInTurn()
     {
         showing = true;
 
         yield return new WaitForSeconds(delay);
+
+        anim.SetBool("Shown", true);
+
+        yield return new WaitForSeconds(delay);
+
         for (int a = 0; a < children.Length; a++)
         {
             children[a].SetBool("Shown", true);
@@ -54,6 +78,11 @@ public class SerialUISummoner : MonoBehaviour
         showing = false;
          
         yield return new WaitForSeconds(delay);
+
+        anim.SetBool("Shown", true);
+
+        yield return new WaitForSeconds(delay);
+
         for (int a = 0; a < children.Length; a++)
         {
             children[a].SetBool("Shown", false);
